33523441937b987ba625198118ed2d884324bc74
scrimz <isakov.kostik@outlook.com>
2023-11-06 15:51:58 +0300

    mobile menu changed

--- src/components/MobileMenu/MobileMenu.tsx
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link, useNavigate} from "react-router-dom";
+import {Link} from "react-router-dom";
 import {useTranslation} from "react-i18next";
 import {useCookies} from "react-cookie";
 import useLocalStorageState from "use-local-storage-state";
@@ -19,31 +19,56 @@ export const MobileMenu: React.FC<MobileMenuProps> = ({ setSelectPropertyTypeDia
     const { isOpen, toggleMenu, updateBurgerIcon } = useMobileMenu()
 
     const closeMenu = (): void => {
-            toggleMenu();
-            updateBurgerIcon();
+        toggleMenu();
+        updateBurgerIcon();
     }
 
+    const [isMenuVisible, setMenuVisible] = React.useState(false);
     const menuAnimation = useSpring({
-        from: { transform: 'translate3d(0, -100%, 0) scale(0.5)', opacity: 0 },
+
+        from: {
+            height: '0%',
+            opacity: 0,
+        },
         to: {
-            transform: isOpen ? 'translate3d(-7%, 0, 0) scale(1)' : 'translate3d(-50%, -100%, 0) scale(0.5)',
+            height: isOpen ? '100%' : '0%',
+            opacity: isOpen? 1 : 0.7,
+        },
+        onRest: () => {
+            if (!isOpen) {
+                setMenuVisible(true);
+            }
+        },
+        config: { mass: 2, tension: 600, friction: 100 },
+        duration: 350
+    });
+
+    const menuNavAnimation = useSpring({
+        from: {
+            transform: 'translateX(-100%)',
+            height: '0%',
+            opacity: 0,
+        },
+        to: {
+            transform: isOpen? 'translateX(0%) scale(1)' : 'translateX(-100%) scale(0.5)',
+            height: isOpen ? '100%' : '0%',
             opacity: isOpen ? 1 : 0,
         },
-        config: { mass: 8, tension: 700, friction: 100 },
+        config: { mass: 7, tension: 600, friction: 100 },
         duration: 350
     });
 
     return (
-        <animated.div className="mobile-menu" style={menuAnimation}>
+        <animated.div className="mobile-menu" style={{ display: isMenuVisible ? 'flex' : 'none', ...menuAnimation}}>
             <div className={'blur'}/>
             <nav className={'mobile-menu__content'}>
                 <ul className="mobile-menu__list">
-                    <li>
+                    <animated.li style={menuNavAnimation}>
                         <Link to="/allListings" className={styles.navLink} onClick={() => closeMenu()}>
                             {t("header.buy")}
                         </Link>
-                    </li>
-                    <li>
+                    </animated.li>
+                    <animated.li style={menuNavAnimation}>
                         <div className={styles.navLink}
                              onClick={() => {
                                  setSelectPropertyTypeDialogVisible(true);
@@ -51,21 +76,27 @@ export const MobileMenu: React.FC<MobileMenuProps> = ({ setSelectPropertyTypeDia
                              }}>
                             {t("header.sell")}
                         </div>
-                    </li>
+                    </animated.li>
                     {cookies.id && (
-                        <Link to="/myListings" className={styles.navLink} onClick={() => closeMenu()}>
-                            {t("header.my_listings")}
-                        </Link>
+                        <animated.li>
+                            <Link to="/myListings" className={styles.navLink} onClick={() => closeMenu()}>
+                                {t("header.my_listings")}
+                            </Link>
+                        </animated.li>
                     )}
                     {info && info.role === "admin" && (
-                        <Link to="/users" className={styles.navLink} onClick={() => closeMenu()}>
-                            {t("header.users")}
-                        </Link>
+                        <animated.li>
+                            <Link to="/users" className={styles.navLink} onClick={() => closeMenu()}>
+                                {t("header.users")}
+                            </Link>
+                        </animated.li>
                     )}
                     {info && (info.role === "admin" || info.role === "moderator") && (
-                        <Link to="/moderation" className={styles.navLink} onClick={() => closeMenu()}>
-                            {t("header.moderation")}
-                        </Link>
+                        <animated.li>
+                            <Link to="/moderation" className={styles.navLink} onClick={() => closeMenu()}>
+                                {t("header.moderation")}
+                            </Link>
+                        </animated.li>
                     )}
                 </ul>
             </nav>
