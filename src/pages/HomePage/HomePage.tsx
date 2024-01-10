857bdfa874ab10f174e30f1f6af19357df8171b7
scrimz <isakov.kostik@outlook.com>
2024-01-10 13:43:39 +0300

    added link from homepg to catalog

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import {useTranslation} from "react-i18next";
+import { Link } from 'react-router-dom';
 import styles from './HomePage.module.scss';
-import backgrd1 from '/images/background1.jpg';
 
 export const HomePage = () => {
     const {t} = useTranslation();
     return (
         <div className={styles.homePage}>
-            <div className={styles.headBlock} style={{backgroundImage: `url(/images/background1.jpg)`}}>
+            <section className={styles.headBlock} style={{backgroundImage: `url(/images/background1.jpg)`}}>
                 <h1>{t("home.greeting")}</h1>
                 <p>{t("home.slogan")}</p>
+                <div className={styles.moveToListingsButton}>
+                    <Link to="/allListings">
+                        {t("home.toListings")}
+                    </Link>
+                </div>
                 <img src="/images/logo2.png" alt="HomeCity Logo"/>
                 <p className={styles.info}>{t("home.info")}</p>
-            </div>
-            <div className={styles.middleBlock}>
-                
-            </div>
+            </section>
+            <section className={styles.middleBlock}>
+
+            </section>
         </div>
     );
 };
